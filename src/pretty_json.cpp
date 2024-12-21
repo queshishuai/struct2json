62d368d906b39c5f4772470f5ee3e49a063ae0f9
queshishuai <39702464+queshishuai@users.noreply.github.com>
2024-12-21 17:06:39 +0800

    Update pretty_json.cpp
    数字数组并列输出

--- src/pretty_json.cpp
+++ src/pretty_json.cpp
@@ -70,20 +70,48 @@ void pretty_json_string(std::string &str, std::string &cout)
     int indentation_length = 2;
     int offset = 0;
     std::string indent;
+    bool flag = 0;//左括号中是数字，不回车，[0,不回车;1,回车]
     for (size_t i = 0; i < str.size() - 1; ++i)
     {
         cout << str[i];
         switch (str[i])
         {
         case '{':
-        case '[':
             offset = indentation_length;
             cout << '\n';
             indent.append(offset, ' ');
             cout << indent;
             break;
+        case '[':
+            if (('0' <= str[i+1]) && (str[i+1] <= '9'))
+            {
+                flag = 1;
+            }
+            else
+            {
+                offset = indentation_length;
+                cout << '\n';
+                indent.append(offset, ' ');
+                cout << indent;
+            }
+            break;
         case '}':
+            if (',' == str[i + 1])
+            {
+            }
+            else
+            {
+                offset = indentation_length;
+                cout << '\n';
+                indent.resize(indent.size() - offset);
+                cout << indent;
+            }
+            break;
         case ']':
+            if(('0' <= str[i-1]) && (str[i-1] <= '9'))
+            {
+                flag = 0;
+            }
             if (',' == str[i + 1])
             {
             }
@@ -96,11 +124,17 @@ void pretty_json_string(std::string &str, std::string &cout)
             }
             break;
         case ',':
-            cout << '\n';
-            cout << indent;
+            if (0 == flag)
+            {
+                cout << '\n';
+                cout << indent;
+            }
+            else
+            {
+            }
             break;
         default:
-            if ((']' == str[i + 1]) || ('}' == str[i + 1]))
+            if (((']' == str[i + 1])&&(('0'>str[i]) || (str[i]>'9'))) || ('}' == str[i + 1]))
             {
                 offset = indentation_length;
                 cout << '\n';
@@ -111,5 +145,6 @@ void pretty_json_string(std::string &str, std::string &cout)
         }
     }
     cout << '}';
+    cout << '\n';
     return;
 }
